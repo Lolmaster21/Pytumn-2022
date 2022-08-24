8a2c9feed517800596e5752809620eaf547d76d1
Lolmaster21 <78552751+Lolmaster21@users.noreply.github.com>
2022-08-24 09:36:54 -0600

    Update Division Check.py

--- Division Check.py
+++ Division Check.py
@@ -1,3 +1,4 @@
+#Number boogers
 import math
 
 num = 0
@@ -11,3 +12,34 @@ else:
     print("SQUARELS")
 
 
+
+
+
+
+#NUMBER 2 Counting 
+t=10    
+while t<= 30:
+  print (t)
+  t+=5
+   
+   
+#Number 3 cords
+  
+#function definition-----------------
+
+def midpoint(x1, y1, x2, y2): 
+    print ((x1+x2)/2,",", (y1+y2)/2)
+#------------------------------------
+
+print("enter first coordinate:")
+a=int(input())
+b=int(input())0
+print("enter second coordinate:")
+c=int(input())
+d=int(input())
+
+#function call
+midpoint(a,b,c,d)
+
+
+#Nuember forth are you a cool kids????
