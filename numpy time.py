dc0ca1cc96ee5409d11ae1a67b1859824531ea13
Lolmaster21 <78552751+Lolmaster21@users.noreply.github.com>
2022-08-29 08:04:23 -0600

    Create numpy time.py

---
+++ numpy time.py
@@ -0,0 +1,15 @@
+import numpy
+a = numpy.array([[1,2,3],
+                 [4,5,6]])
+
+b = numpy.array([[10,11,12],
+                 [13,14,15]])
+
+c = a + b
+d = a - b
+e = a * b
+
+print(c)
+print(d)
+print(e)
+
