bd1ef0c90a362bbe59d163ba74e85073cbcec550
Lolmaster21 <78552751+Lolmaster21@users.noreply.github.com>
2022-09-01 09:45:39 -0600

    Create Rotating nump.py

---
+++ Rotating nump.py
@@ -0,0 +1,17 @@
+import numpy
+
+m = numpy.array([[1,2],
+                 [3,4]], int)
+
+m = numpy.rot90(m)
+print(m, "\n")
+
+m = numpy.rot90(m,2)
+print(m, "\n")
+
+
+
+m = numpy.arange(8).reshape((2,2,2))
+numpy.rot90(m, 1, (1,2))
+
+print(m)
