2b3fe450b57d1a91b6caa06da5adabe11c91ace9
Lolmaster21 <78552751+Lolmaster21@users.noreply.github.com>
2022-09-02 09:42:58 -0600

    Create Quiz1?.py

---
+++ Quiz1?.py
@@ -0,0 +1,16 @@
+for i in range (8, 28, 4):
+  print(i)
+  print("New loop-----------------------------------")
+for j in range (80, 71, -1):
+  print(j)
+ 
+num = 0
+num = (int(input("Infinite bonks?")))
+
+j=0
+while (j != num) :
+    print ("BONK")
+    j+=1
+
+
+
