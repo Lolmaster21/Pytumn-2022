d0f10105d6dc0e5f23e9c393c5c55b0f734183cd
Lolmaster21 <78552751+Lolmaster21@users.noreply.github.com>
2022-10-05 08:16:07 -0600

    Create Tuples.py

---
+++ Tuples.py
@@ -0,0 +1,13 @@
+import pygame
+import time
+pygame.init()
+pygame.display.set_caption("Data structures")
+
+
+FRANDS = ("Jesse","Ricardo","Kevin","Simon","Random name")
+
+print(FRANDS)
+print(FRANDS[2])
+
+
+pygame.quit
